a4a74863c4640fb2e64c0f3aad7401aae62bdcea
Michael Gibbons <mgibbons2@luc.edu>
2016-02-03 16:53:51 -0600

    Created constructor and some getters/setters for twittermessage objects

--- src/TwitterMessage.java
+++ src/TwitterMessage.java
@@ -1,6 +1,37 @@
 import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TwitterMessage {
-	private String mention;
-	private Array mentions;
+	private String tweetText;
+	private List<String> words;
+	private List<String> mentions;
+	private List<String> links;
+	private LocalDateTime dateTime;
+	
+	public TwitterMessage(String text){
+		if(tweetText.isEmpty() || this.tweetText == null){
+			tweetText = text;
+		}
+		dateTime = LocalDateTime.now();
+		words = new ArrayList<String>(Arrays.asList(text.split(" ")));
+	}
+	public TwitterMessage(){
+		dateTime = LocalDateTime.now();
+	}
+	public LocalDateTime getDate(){
+		return this.dateTime;
+	}
+	public void setDate(){
+		this.dateTime = LocalDateTime.now();
+	}
+	public String getTweetText(){
+		return tweetText;
+	}
+	public void setTweetText(String text){
+		this.tweetText = text;
+	}
+	
 }
