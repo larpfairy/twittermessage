8bc247313f3ed48dfdef3d0f348e8c0870ffd5b1
larpfairy <michaelgibs123@gmail.com>
2016-02-03 19:56:30 -0600

    Created functionality to check url's and to parse mentions and add them to list

--- src/TwitterMessage.java
+++ src/TwitterMessage.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,13 +20,22 @@ public class TwitterMessage {
 			tweetText = text;
 		}
 		dateTime = LocalDateTime.now();
+		text = text.trim();
 		words = new ArrayList<String>(Arrays.asList(text.split(" ")));
+		for(int i = 0; i < words.size(); i++){
+			if(words.get(i).charAt(0) == '@'){
+				mentions.add(words.get(i));
+			}
+			if(pingUrl(words.get(i)) == true){
+				links.add(words.get(i));
+			}
+		}
 	}
 	public TwitterMessage(){
 		dateTime = LocalDateTime.now();
 	}
-	public LocalDateTime getDate(){
-		return this.dateTime;
+	public String getDate(){
+		return this.dateTime.toString();
 	}
 	public void setDate(){
 		this.dateTime = LocalDateTime.now();
@@ -33,5 +46,28 @@ public class TwitterMessage {
 	public void setTweetText(String text){
 		this.tweetText = text;
 	}
+	public List<String> getMentions(){
+		return mentions;
+	}
+	public static boolean pingUrl(final String address) {//i found this code on stack overflow, it checks if a string is a valid URL.
+		try {
+		final URL url = new URL("http://" + address);
+		final HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+		urlConn.setConnectTimeout(1000 * 10); // mTimeout is in seconds
+		final long startTime = System.currentTimeMillis();
+		urlConn.connect();
+		final long endTime = System.currentTimeMillis();
+		if (urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+		System.out.println("Time (ms) : " + (endTime - startTime));
+		System.out.println("Ping to "+address +" was success");
+		return true;
+		}
+		} catch (final MalformedURLException e1) {
+		e1.printStackTrace();
+		} catch (final IOException e) {
+		e.printStackTrace();
+		}
+		return false;
+		}
 	
 }
